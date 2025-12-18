5e7eb05b5b55b7b4c53eab44d4d1d3aef790ddef
Felix Nagele <43892671+felixnagele@users.noreply.github.com>
2025-12-19 00:36:46 +0100

    Implement basic realloc and add unit test for it

--- src/realloc.cpp
+++ src/realloc.cpp
@@ -5,9 +5,26 @@ namespace mem
 
 void* realloc(void* ptr, size_t new_size)
 {
-    (void)ptr;
-    (void)new_size;
-    return nullptr;
+    if (ptr == nullptr)
+    {
+        return malloc(new_size);
+    }
+
+    if (new_size == 0)
+    {
+        free(ptr);
+        return nullptr;
+    }
+
+    void* new_ptr = malloc(new_size);
+    if (new_ptr == nullptr)
+    {
+        return nullptr;
+    }
+
+    free(ptr);
+
+    return new_ptr;
 }
 
 } // namespace mem
