ce42efe3eff956b9dabbd5f81afb526f14e08bd9
Felix Nagele <43892671+felixnagele@users.noreply.github.com>
2025-12-19 12:45:49 +0100

    Implement advanced malloc features (#31)
    ## Description
    
    Implement advanced malloc features.
    
    ## Issues
    
    <!-- use if this PR fully resolves the issue -->
    
    Closes #29 
    
    <!-- use if this PR is linked but should not close the issue -->
    
    Related: #<issue-number>
    
    ## Testing
    
    <!-- Steps or notes on how reviewers can verify. -->

--- include/malloc_from_scratch/memory_internal.h
+++ include/malloc_from_scratch/memory_internal.h
@@ -22,13 +22,22 @@ inline size_t total_memory_allocated = 0;
 inline pthread_mutex_t allocator_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 constexpr size_t BLOCK_METADATA_SIZE = sizeof(MemoryBlock);
-constexpr size_t MIN_BLOCK_SIZE = 16;
+constexpr size_t BLOCK_SIZE = 16;
+constexpr size_t CHUNK_SIZE = 65536;
 
+// malloc
+void* increaseHeap(size_t size);
+MemoryBlock* findLargeEnoughFreeMemoryBlock(MemoryBlock** block_list_head, size_t size);
+void* splitFreeMemoryBlockIfPossible(MemoryBlock* new_block, size_t size);
+void* getMemoryBlockSplitAddress(MemoryBlock* new_block, size_t size);
 void insertMemoryBlockAtEnd(MemoryBlock** block_list_head, MemoryBlock* new_block);
+
+// helpers
 void* getPayloadAddress(MemoryBlock* block);
 MemoryBlock* getMetadata(void* payload_address);
-void* increaseHeap(size_t size);
-void* decreaseHeap(MemoryBlock* block_heap_end, size_t size);
+MemoryBlock* getMemoryBlockFromAddress(void* address);
+size_t getSizeOfAllocatedMemoryBlock(MemoryBlock* block);
+void* getErrorCodeInVoidPtr(size_t error_code);
 
 } // namespace internal
 } // namespace mem
