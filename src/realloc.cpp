a389f85965e235547a0f0bfb1406e5955ba9818f
Felix Nagele <43892671+felixnagele@users.noreply.github.com>
2025-12-20 02:08:00 +0100

    Add advanced calloc and realloc features

--- src/realloc.cpp
+++ src/realloc.cpp
@@ -1,4 +1,8 @@
 #include "malloc_from_scratch/memory_allocator.h"
+#include "malloc_from_scratch/memory_internal.h"
+
+#include <cstdlib>
+#include <cstring>
 
 namespace mem
 {
@@ -16,11 +20,40 @@ void* realloc(void* ptr, size_t new_size)
         return nullptr;
     }
 
+    if (!internal::isPointerInHeap(ptr))
+    {
+        return nullptr;
+    }
+
+    internal::MemoryBlock* old_block = internal::getMetadata(ptr);
+    if (!internal::isValidBlock(old_block))
+    {
+        return nullptr;
+    }
+
+    if (!internal::checkCanary(old_block))
+    {
+        exit(-1);
+    }
+
+    size_t old_size = old_block->size_;
+
     void* new_ptr = malloc(new_size);
     if (new_ptr == nullptr)
     {
         return nullptr;
     }
+
+    size_t copy_size;
+    if (old_size < new_size)
+    {
+        copy_size = old_size;
+    }
+    else
+    {
+        copy_size = new_size;
+    }
+    memcpy(new_ptr, ptr, copy_size);
 
     free(ptr);
 
